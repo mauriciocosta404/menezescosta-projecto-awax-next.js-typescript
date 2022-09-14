d07eec616cc11d5da6f481efaad870ffcd19cc78
menezescosta <mc611641@gmail.com>
2022-09-14 22:53:50 +0100

    configurando o priceMock

---
+++ src/components/mock/priceMock.ts
@@ -0,0 +1,18 @@
+export const priceMock=[
+    {
+        name:'EXPRESS',
+        price:9.99
+    },
+    {
+        name: 'STANDART',
+        price: 14.99
+    },
+    {
+        name: 'LUXE',
+        price: 19.99
+    },
+    {
+        name: 'PREMIUM',
+        price: 24.99
+    }
+];
