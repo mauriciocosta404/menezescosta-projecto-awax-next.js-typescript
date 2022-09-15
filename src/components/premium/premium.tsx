b320a8adc659edea3f75dc7d3b61762918169e1f
menezescosta <mc611641@gmail.com>
2022-09-15 20:31:52 +0100

    adicionando configuração do premium

---
+++ src/components/premium/premium.tsx
@@ -0,0 +1,13 @@
+import * as C from './style';
+
+const Premium=()=>{
+    return(
+         <C.Container>
+            <div className="title">
+                <h2>PREMIUM</h2>
+                <span>EVEN MORE FEACTURES AVAILABLE</span>
+            </div>
+         </C.Container>
+    )
+}
+export default Premium;
